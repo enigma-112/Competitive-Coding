9d2c61b8306f7feb2b37f94cbb3b08ad6089afc1
Kundan Kumar <rrd51510@gmail.com>
2019-02-13 00:06:08 +0530

    lcm

---
+++ Maths/lcm.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int GCD(int a,int b){
+
+if(b==0){
+	return a;
+}
+	
+return GCD(b,a%b);
+
+}
+
+int LCM(int a,int b){
+	return (a*b)/GCD(a,b);
+}
+
+int main(){
+
+
+int a,b;
+cout<<" Enter the two no.s : ";
+cin>>a>>b;
+
+int lcm = LCM( a, b);
+cout<<"LCM of "<< a<<" and "<<b <<" is "<<lcm<<endl;
+
+
+
+}
