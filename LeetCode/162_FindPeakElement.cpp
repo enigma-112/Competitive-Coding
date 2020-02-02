72b7469b25f429dbbe408f8002ac6cf33817b378
enigma-112 <rrd51510@gmail.com>
2020-02-03 03:47:56 +0530

    a

---
+++ LeetCode/162_FindPeakElement.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/find-peak-element/
+
+// 162. Find Peak Element
+
+// T : O(log N)
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n= nums.size();
+        int low = 0,high = n-1;
+        while(low<high){
+            int mid = (low+high)/2;
+            if(nums[mid] > nums[mid+1] ){
+                high=mid;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
+        
+    }
+};
